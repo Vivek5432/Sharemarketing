0e3f15496333fae95461f7b333192a915533c20b
57694_Prathmesh <prathmeshligade49@gmail.com>
2022-03-13 08:02:23 +0530

    new code is added new branch

--- src/main/java/Confi/Confi.java
+++ src/main/java/Confi/Confi.java
@@ -2,4 +2,7 @@ package Confi;
 
 public class Confi {
 
+	public static String chromedriverpath="F:\\SOFTWARE TESTING\\Chrome  Driver\\Chrome98\\chromedriver.exe";
+	 
+	public static String firefoxpath="F:\\SOFTWARE TESTING\\FIREFOX DRIVER\\geckodriver.exe";
 }
